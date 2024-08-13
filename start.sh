2afbfad21e92d5b7556dbe3487aed42a6dd934fa
Stars <159614502+Stars-niu@users.noreply.github.com>
2024-08-13 17:26:07 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 git lfs install
 
 # 克隆仓库
-git clone https://github.com/your-username/ByteBrain.git
+git clone https://github.com/Stars-niu/ByteBrain.git
 
 # 安装Streamlit
 pip install streamlit==1.24.0
@@ -13,4 +13,4 @@ pip install streamlit==1.24.0
 cd ByteBrain
 
 # 启动Streamlit应用
-streamlit run demo/app.py --server.address 127.0.0.1 --server.port 6006
+streamlit run app.py --server.address 127.0.0.1 --server.port 1001
